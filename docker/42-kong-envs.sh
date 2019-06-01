62c0b3fb5becfb87a28893f48b037c3d3db7f986
Raimon Grau <raimonster@gmail.com>
2019-06-01 22:22:46 +0100

    feat(bash) add a basic useful ps1
    This commit creates a basic PS1 which uses GOJIRA_KONG_PATH replacing
    host.
    
    Creating a .gojirarc that is sourced from .profile makes it that in
    the future (if needed) gojira could be pulling regularly this file
    without messing user .bashrc configs.

--- docker/42-kong-envs.sh
+++ docker/42-kong-envs.sh
@@ -1,3 +1,4 @@
+export PS1="\\u@$GOJIRA_HOSTNAME:\\w\\$ "
 eval `luarocks path 2> /dev/null`
 LUA_PATH="$KONG_PATH/?.lua;$KONG_PATH/?/init.lua;$KONG_PLUGIN_PATH/?.lua;$KONG_PLUGIN_PATH/?/init.lua;$LUA_PATH"
 PATH="$PATH:$KONG_PATH/bin"
--- docker/docker-compose.yml.sh
+++ docker/docker-compose.yml.sh
@@ -76,6 +76,7 @@ cat << EOF
       # name for the db server, we need to set up the DNS resolver, is set
       # to 8.8.8.8 on the spec conf
       KONG_TEST_DNS_RESOLVER: 127.0.0.11
+      GOJIRA_HOSTNAME: ${GOJIRA_HOSTNAME:-${GOJIRA_KONG_PATH/*\//}}
 
     restart: on-failure
     networks:
