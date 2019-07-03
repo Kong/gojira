397d93f1aad1e3ae569a9819c96852218b143d94
eskerda <eskerda@gmail.com>
2019-07-03 19:32:18 +0200

    fix(gojira) look for more general OPENRESTY
    kong just stopped using OPENRESTY_LATEST or _BASE, so it's safe to
    assume we can rely on looking for OPENRESTY only
    
    on kong-ee, both _BASE and _LATEST point to the same version, so it's
    also safe we are going to remove it

--- gojira.sh
+++ gojira.sh
@@ -308,7 +308,7 @@ function image_name {
   local travis_yaml=$GOJIRA_KONG_PATH/.travis.yml
   LUAROCKS=${LUAROCKS:-$(yaml_find $travis_yaml LUAROCKS)}
   OPENSSL=${OPENSSL:-$(yaml_find $travis_yaml OPENSSL)}
-  OPENRESTY=${OPENRESTY:-$(yaml_find $travis_yaml OPENRESTY_BASE)}
+  OPENRESTY=${OPENRESTY:-$(yaml_find $travis_yaml OPENRESTY)}
 
   if [[ -z $LUAROCKS || -z $OPENSSL || -z $OPENRESTY ]]; then
     err "${GOJIRA}: Could not guess version dependencies in" \
