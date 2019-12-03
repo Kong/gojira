6a0dbb7808ab5b7573a98ae9942b97be2560216b
Raimon Grau <raimonster@gmail.com>
2019-12-03 12:07:53 +0000

    feat(*) fix PATH for running production images

--- docker/42-kong-envs.sh
+++ docker/42-kong-envs.sh
@@ -1,6 +1,6 @@
 eval `luarocks path 2> /dev/null`
 LUA_PATH="$KONG_PATH/?.lua;$KONG_PATH/?/init.lua;$KONG_PLUGIN_PATH/?.lua;$KONG_PLUGIN_PATH/?/init.lua;$LUA_PATH"
-PATH="$PATH:$KONG_PATH/bin"
+PATH="$PATH:$KONG_PATH/bin:/usr/local/openresty/bin/"
 
 function make_prompt {
   local res=$?
