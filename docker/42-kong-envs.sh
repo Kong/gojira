c841f708df0c7cb8b5d5d251fff35551ff599ef2
eskerda <eskerda@gmail.com>
2020-04-16 15:58:03 +0200

    fix(gojira) sh compatible rc file

--- docker/42-kong-envs.sh
+++ docker/42-kong-envs.sh
@@ -2,15 +2,18 @@ eval `luarocks path 2> /dev/null`
 LUA_PATH="$KONG_PATH/?.lua;$KONG_PATH/?/init.lua;$KONG_PLUGIN_PATH/?.lua;$KONG_PLUGIN_PATH/?/init.lua;$LUA_PATH"
 PATH="$PATH:$KONG_PATH/bin:/usr/local/openresty/bin/"
 
-function make_prompt {
-  local res=$?
-  local usr path
+make_prompt (){
+  _prompt_res=$?
 
-  [ "$res" = 0 ] && hint="\[\033[1;34m\]" || hint="\[\033[1;91m\]"
-  [ "$PWD" = "$HOME" ] && path="~" || path=$PWD
-  [ "$USER" = "root" ] && usr="#" || usr="$"
+  # This prompt has only been tested with bash. Remove this if it works
+  # anywhere else
+  if [ -z "$BASH_VERSION" ]; then return; fi
 
-  PS1="\[\e[00m\]$hint[$GOJIRA_PREFIX:\[\033[1;92m\]$path$hint]$usr\[\033[00m\] "
+  [ "$_prompt_res" = 0 ] && _prompt_hint="\[\033[1;34m\]" || _prompt_hint="\[\033[1;91m\]"
+  [ "$PWD" = "$HOME" ] && _prompt_path="~" || _prompt_path=$PWD
+  [ "$USER" = "root" ] && _prompt_usr="#" || _prompt_usr="$"
+
+  PS1="\[\e[00m\]$_prompt_hint[$GOJIRA_PREFIX:\[\033[1;92m\]$_prompt_path$_prompt_hint]$_prompt_usr\[\033[00m\] "
 }
 
 USER=`whoami`
