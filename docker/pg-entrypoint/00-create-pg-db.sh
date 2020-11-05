e49dc716ea04ed9d87504263d31b081285499702
Raimon Grau <raimonster@gmail.com>
2020-11-05 11:17:23 +0000

    feat(db) track pg statements analytics

--- docker/pg-entrypoint/00-create-pg-db.sh
+++ docker/pg-entrypoint/00-create-pg-db.sh
@@ -7,6 +7,10 @@ log_statement = 'all'
 log_disconnections = off
 log_duration = on
 log_min_duration_statement = -1
+shared_preload_libraries = 'pg_stat_statements'
+track_activity_query_size = 2048
+pg_stat_statements.track = all
+pg_stat_statements.max = 10000
 " >>$pg_conf_file
 
 for database in $(echo $POSTGRES_DBS | tr ',' ' '); do
