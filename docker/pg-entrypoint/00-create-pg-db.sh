47e2e8975dd3d947bc912af8406d9d63dfd3b3e9
Murillo Paula <murillo@murillopaula.com>
2020-12-01 01:09:49 +0000

    wip

--- docker/pg-entrypoint/00-create-pg-db.sh
+++ docker/pg-entrypoint/00-create-pg-db.sh
@@ -11,12 +11,20 @@ shared_preload_libraries = 'pg_stat_statements'
 track_activity_query_size = 2048
 pg_stat_statements.track = all
 pg_stat_statements.max = 10000
+password_encryption = scram-sha-256
 " >>$pg_conf_file
+
+pg_hba_conf_file=/var/lib/postgresql/data/pg_hba.conf
+rm $pg_hba_conf_file
+echo "\
+host all all all scram-sha-256
+" >>$pg_hba_conf_file
 
 for database in $(echo $POSTGRES_DBS | tr ',' ' '); do
   echo "Creating database $database"
   psql -U $POSTGRES_USER <<-EOSQL
     CREATE DATABASE $database;
+    ALTER ROLE $POSTGRES_USER WITH LOGIN PASSWORD 'secret';
     GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 done
