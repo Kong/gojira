f0759d365dc06e7eda5f5bd5e588924b2e3bf6d9
Alan Boudreault <alan@alanb.ca>
2022-01-11 10:04:38 -0500

    fix(redis) ensure the redis password is used when creating the cluster

--- docker/redis-cluster.sh
+++ docker/redis-cluster.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-unset redis_password
+unset redis_server_password
+unset redis_connect_password
+
+if [ -n "$REDIS_PASSWORD" ]; then
+  redis_server_password="--requirepass $REDIS_PASSWORD"
+  redis_connect_password="-a $REDIS_PASSWORD"
+fi
 
 for i in $(seq 0 $((REDIS_CLUSTER_NODES - 1)));
 do
   port=$((7000+$i))
-  if [ -n "$REDIS_PASSWORD" ]; then
-    redis_password="--requirepass $REDIS_PASSWORD"
-  fi
-  echo $redis_password
-  sh -c "nohup redis-server --port $port --cluster-enabled yes --cluster-config-file redis-slave-$port-nodes.conf --cluster-node-timeout 5000 $redis_password > nohup-$port.txt &"
+  sh -c "nohup redis-server --port $port --cluster-enabled yes --cluster-config-file redis-slave-$port-nodes.conf --cluster-node-timeout 5000 $redis_server_password > nohup-$port.txt &"
 done
 
 sleep 5
@@ -18,7 +20,7 @@ then
   IP=$(hostname -i | awk '{print $1}');
 fi
 
-yes yes | redis-cli --cluster create $(for i in $(seq 0 $((REDIS_CLUSTER_NODES - 1))); do port=$((7000+$i)); printf "${IP}:$port "; done) --cluster-replicas 1
+yes yes | redis-cli --cluster create $(for i in $(seq 0 $((REDIS_CLUSTER_NODES - 1))); do port=$((7000+$i)); printf "${IP}:$port "; done) --cluster-replicas 1 $redis_connect_password
 
 tail -f /dev/null
 
