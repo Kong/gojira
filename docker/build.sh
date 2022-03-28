7e84555d299c29464e63d5d905933f75bf96a9cf
Qi <add_sp@outlook.com>
2022-03-28 18:39:06 +0800

    check arch

--- docker/build.sh
+++ docker/build.sh
@@ -51,9 +51,19 @@ function make_kong_ngx_module {
 }
 
 function make_libgmp {
+  local kernel
+  local machine
+  machine="$(uname -m)"
+
+  local build_arg="--build=x86_64-linux-gnu"
+  if [[ "$machine" = "aarch64" ]] || [[ "$machine" == "arm64" ]]; then
+    build_arg=""
+  fi
+
   (
     cd "${LIBGMP_INSTALL}" || return
     ./configure \
+      "$build_arg" \
       --enable-static=no \
       --prefix="${LIBGMP_INSTALL}"
     make install
