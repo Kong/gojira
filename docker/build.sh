30a2954f6c3ee76a42045419fca9e4a911398dd9
Qi <add_sp@outlook.com>
2022-03-28 22:39:43 +0800

    fix(build): adjust build arguments for libgmp (#41)
    This corrects build issues for M1 chips

--- docker/build.sh
+++ docker/build.sh
@@ -51,10 +51,18 @@ function make_kong_ngx_module {
 }
 
 function make_libgmp {
+  local machine
+  machine="$(uname -m)"
+
+  local build_arg="--build=x86_64-linux-gnu"
+  if [[ "$machine" == "aarch64" ]] || [[ "$machine" == "arm64" ]]; then
+    build_arg=""
+  fi
+
   (
     cd "${LIBGMP_INSTALL}" || return
     ./configure \
-      --build=x86_64-linux-gnu \
+      "$build_arg" \
       --enable-static=no \
       --prefix="${LIBGMP_INSTALL}"
     make install
